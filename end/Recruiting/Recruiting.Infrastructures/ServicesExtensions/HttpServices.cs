a3f325d8ad37e23d7ec51a4d17942dd73904cc1d
julienlesur <julienlesur@hotmail.fr>
2020-08-10 16:05:15 -0400

    Add ActiveMenuTabHelper
    Add a taghelper to select the active menu

---
+++ end/Recruiting/Recruiting.Infrastructures/ServicesExtensions/HttpServices.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class HttpServices
+    {
+        public static IServiceCollection AddHttpServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            return services;
+        }
+    }
+}
