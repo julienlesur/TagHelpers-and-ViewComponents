6dc32fd785951fe6d89aa5f31b16e410eab5ec85
julienlesur <julienlesur@hotmail.fr>
2020-08-12 17:18:41 -0400

    display applicants list in details
    Tomorrow, I'll put some style and manage clicks

---
+++ end/Recruiting/Recruiting.Web/ViewComponents/ApplicantsShortListViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Recruiting.BL.Models;
+using Recruiting.BL.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recruiting.Web.ViewComponents
+{
+    public class ApplicantsShortListViewComponent : ViewComponent
+    {
+        private readonly IApplicantService _applicantService;
+
+        public ApplicantsShortListViewComponent(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(
+            int applicantId, string jobReference)
+        {
+            IEnumerable<Applicant> applicants = await _applicantService.GetApplicantList(jobReference);
+
+            return View(applicants);
+        }
+    }
+}
